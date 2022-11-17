3c2495b8fdea7fcd85b63fed486a7430d8b90f85
Phu Lam <43253532+PML75@users.noreply.github.com>
2022-11-16 19:01:07 -0800

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -5,18 +5,29 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
-        self.goto(0, 260)
-        self.write(f"Score: {self.score}", align="center", font=("Courier", 24, "normal"))
+        self.goto(0, 270)
         self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Courier", 18, "normal"))
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Courier", 24, "normal"))
+        self.write(f"Score: {self.score}", align="center", font=("Courier", 18, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Courier", 24, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
 
